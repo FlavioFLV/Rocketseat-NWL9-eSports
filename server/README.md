311649c4a990a45f704c79e91bf302e43c919643
Flávio Prado Da Cunha <409.992.158-17@AEISSP.LOCAL>
2022-09-21 16:27:55 -0300

    Criando o banco de dados com Prisma.

---
+++ server/README.md
@@ -0,0 +1,29 @@
+# Back-end
+
+## Entidades
+
+### Game
+
+id
+title
+bannerUrl
+
+### Ads
+
+id
+idGame
+name
+yearsplayling
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+## Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- buscar discord pelo ID do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
-
 const app = express();
 
+// HTTP methods / API RESTFul / HTTP Codes
 
+app.get('/games', (request, response) => {
+    return response.json([]);
+})
 
-app.get("/ads", (request, response) => {
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id;
+    
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -12,6 +21,12 @@ app.get("/ads", (request, response) => {
         { id: 4, name: 'Anúncio 4' },
         { id: 5, name: 'Anúncio 5' },
     ])
+});
+
+app.get('/games/:id/discord', (request, response) => {
+    // const adId = request.params.id;
+    
+    return response.json([])
 });
 
 app.listen(3333);
