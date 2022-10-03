29081f30e0d8da9ca8d6d72ae29c16fdd9b90522
FlavioFLV <flavioprado78@gmail.com>
2022-10-03 08:49:17 -0300

    Desenvolvendo Modal de criação de Ads

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -6,7 +6,6 @@ export function Input(props: InputProps) {
     return (
     <input
         {...props}
-        id="game"
         className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
     />
     )
